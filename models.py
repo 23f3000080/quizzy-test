2f756eb7a2cda0c4baf8213b11a8e3d79d1a420e
Ankit-Aarya <23f3000080@ds.study.iitm.ac.in>
2025-02-26 23:10:05 +0530

    Created database schema for users, subjects, chapter, question, quiz and quiz_result

--- models.py
+++ models.py
@@ -1,1 +1,81 @@
- 
+from flask_sqlalchemy import SQLAlchemy
+from app import app
+from datetime import datetime
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    qualification = db.Column(db.String(100), nullable=False)
+    dob = db.Column(db.Date, nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+    joined_at = db.Column(db.DateTime, default=datetime.now)
+    updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+
+class Subject(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sub_name = db.Column(db.String(100), nullable=False, unique=True)
+    description = db.Column(db.String(100), nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+
+    #relationship
+    chapters = db.relationship('Chapter', backref='subject', lazy=True, cascade="all, delete-orphan")
+    
+
+class Chapter(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    chapter_name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(100), nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    #foreign-key
+    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'), nullable=False)
+    #relationship for question
+    questions = db.relationship('Question', backref='chapter', lazy=True, cascade="all, delete-orphan")
+
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    option1 = db.Column(db.String(200), nullable=False)
+    option2 = db.Column(db.String(200), nullable=False)
+    option3 = db.Column(db.String(200), nullable=False)
+    option4 = db.Column(db.String(200), nullable=False)
+    correct_option = db.Column(db.String(200), nullable=False)
+    marks = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    #foreign-key
+    chapter_id = db.Column(db.Integer, db.ForeignKey('chapter.id'), nullable=False)
+
+class Quiz(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(100), nullable=True)
+    number_of_questions = db.Column(db.Integer, nullable=False)
+    duration = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+
+    #foreign-key
+    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'), nullable=False)
+    chapter_id = db.Column(db.Integer, db.ForeignKey('chapter.id'), nullable=False)
+
+    #relationship
+    subject = db.relationship('Subject', backref='quizzes', lazy="joined")
+    chapter = db.relationship('Chapter', backref='quizzes', lazy="joined")
+
+class QuizResult(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    score = db.Column(db.Integer, nullable=False)
+    time_taken = db.Column(db.Time, nullable=False)
+    quiz_attempt_date = db.Column(db.DateTime, default=datetime.now)
+
+    #foreign-key
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
